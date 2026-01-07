42ba27ff6546613e13d9d9c67b6f2c8ad11d01a1
brij-1234 <brijk7434@email.com>
2026-01-07 14:02:57 +0530

    port changes

--- src/main.ts
+++ src/main.ts
@@ -39,9 +39,9 @@ async function bootstrap() {
   (BigInt.prototype as any).toJSON = function () {
     return this.toString();
   };
-  await app.listen(process.env.PORT ?? 3000);
+  //await app.listen(process.env.PORT ?? 3000);
   //await app.listen(process.env.PORT || 3000, '0.0.0.0');
-
+await app.listen(process.env.PORT || 3000 ,'0.0.0.0');
   
 }
 bootstrap();
